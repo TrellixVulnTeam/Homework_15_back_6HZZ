85d5ea8f6c005fc3cb965712c17b7521d5b8e6a8
Alexander Grams <cawa.cerber@gmail.com>
2022-06-13 15:46:22 +0500

    final

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 require('./models/models')
+
+const cors = require('cors')
 const router = require('./routers/router')
 
 const sequelize = require('./db');
 const PORT = process.env.PORT
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 app.use('/api/router', router)
 
 const start = async () =>{
